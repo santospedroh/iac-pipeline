19f1eef86cc005179cc5fac28e7cf652f1f573ef
Pedro Santos <pedsantos@uolinc.com>
2022-06-21 21:23:55 -0300

    Ajuste SG

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "game_snake_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.200.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port        = 0
